b1b2fae3acae673ea13061088cdd1d086edb737a
ogre-tab <ogre-tab@users.noreply.github.com>
2018-11-06 21:51:32 -0800

    Updated directory structure and moved main app.py file to root. Updated paths in app.py and whooshPowers.py to use CWD (current working directory) to fix file not found errors. Changed the name of some functions in whooshPowers.py to better describe what the functions do.

---
+++ app.py
@@ -0,0 +1,53 @@
+import os
+
+from flask import Flask, render_template, request
+
+from indexing.whooshPowers import search, checkAndLoadIndex
+
+# template file names
+home_page = "welcome_page.html"
+results_page = "results.html"
+
+# create our flask object
+app = Flask(__name__)
+# change some of our flask settings
+app.template_folder = os.path.join("webUI", "templates")
+app.static_folder = os.path.join("webUI", "static")
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    print('HEya')
+    return render_template(home_page)
+
+
+@app.route('/my-link/')
+def my_link():
+    print('clicked')
+    return 'Click'
+
+
+@app.route('/results/', methods=['GET', 'POST'])
+def results():
+    global indexr
+    if request.method == 'POST':
+        data = request.form
+    else:
+        data = request.args
+
+    keywordquery = data.get('searchterm')
+    test = data.get('test')
+
+    print('Keyword Query is: ' + keywordquery)
+    print('Test Query is: ' + test)
+
+    name, description = search(indexr, keywordquery)
+
+    return render_template(results_page, query=keywordquery, results=zip(name, description))
+
+
+if __name__ == '__main__':
+    global indexr
+
+    indexr = checkAndLoadIndex()
+    app.run(debug=True)
